363cc996ef7d615c2043487bd642386a1d784e28
Ravi Prasai <raviprasai1994@gmail.com>
2021-05-01 02:27:52 -0500

    Images and Toppings

--- pizzas/views.py
+++ pizzas/views.py
@@ -6,13 +6,14 @@ def index(request):
     '''Home page for Pizzas.'''
     return render(request, 'pizzas/index.html')
 
-def pizzas(request, pizzza_id):
+def pizzas(request):
     pizzas = Pizza.objects.order_by('date_added')
     context={'pizzas':pizzas}
     return render(request, 'pizzas/pizzas.html',context)
 
 def pizza(request, pizza_id):
-    pizza = Pizza.objects.get(id=pizza_id)
+    pizza = Pizza.objects.get(id = pizza_id)
+   # image = Pizza.objects.get()
     toppings=pizza.topping_set.order_by('-date_added')
     context={'pizza':pizza, 'toppings':toppings}
     return render(request, 'pizzas/pizza.html',context)
